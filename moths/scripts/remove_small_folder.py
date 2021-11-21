42148b8c01a1e02a9825481cb8b36198cee5ff65
Kay Lamerigts <k.lamerigts@gmail.com>
2021-11-21 17:37:22 +0100

    add remove small folder script

---
+++ moths/scripts/remove_small_folder.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+import typer
+import shutil
+
+remove_small_folder_app = typer.Typer()
+
+
+@remove_small_folder_app.command()
+def remove(path: Path, minimum: int) -> None:
+    for class_path in path.iterdir():
+        num_files = len(list(class_path.iterdir()))
+        if num_files < minimum:
+            shutil.rmtree(class_path)
+            print(f"removed {str(class_path)} with {num_files} files")
+
+
+if __name__ == "__main__":
+    remove_small_folder_app()
