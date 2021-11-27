647e2c91c4dd6d21fd7749bcd2d69704cc64e562
Kay Lamerigts <k.lamerigts@gmail.com>
2021-11-27 13:26:35 +0100

    do average over var not std

--- moths/scripts/calculate_normalization_values.py
+++ moths/scripts/calculate_normalization_values.py
@@ -16,15 +16,16 @@ OUTPUT_FOLDER_NAME = "image_folder"
 
 @norm_values_app.command()
 def calculate_normalization_values(data_path: Path) -> None:
-
     def _calc(file_path: Path) -> Tuple[Tuple[float, float, float], Tuple[float, float, float]]:
         img = np.array(Image.open(file_path).convert("RGB"))
-        return img.mean(axis=(0, 1)), img.std(axis=(0,1))
+        return img.mean(axis=(0, 1)), img.var(axis=(0, 1))
 
     paths = [p for c in data_path.iterdir() for p in c.iterdir()]
     stats = Parallel(n_jobs=24)(delayed(_calc)(p) for p in tqdm(paths))
+    stats = np.array(stats).mean(axis=0)
 
-    print(np.array(stats).mean(axis=0))
+    print("mean:", stats[0].round(2))
+    print("std: ", np.sqrt(stats[1]).round(2))
 
 
 if __name__ == "__main__":
