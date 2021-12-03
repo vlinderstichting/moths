71fb59f11edc5adac30abed79515ccc21ed9a13f
Kay Lamerigts <k.lamerigts@gmail.com>
2021-12-03 13:42:46 +0100

    add nested and listed hyperparameter logging & encapsulate tuning of batch size and lr

--- moths/logging.py
+++ moths/logging.py
@@ -1,9 +1,46 @@
-from omegaconf import DictConfig
+from copy import deepcopy
+
+from omegaconf import DictConfig, OmegaConf
 from pytorch_lightning import Trainer
 from pytorch_lightning.utilities import rank_zero_only
 
 from moths.data_module import DataModule
 from moths.lit_module import LitModule
+
+
+def replace_list_with_dict(d: dict) -> None:
+    for k, v in d.items():
+        if isinstance(v, dict):
+            replace_list_with_dict(v)
+        elif isinstance(v, list):
+            d[k] = {i: v[i] for i in range(len(v))}
+            replace_list_with_dict(d[k])
+
+
+def target_dict_to_string(d: dict):
+    klass = f"{d['_target_'].split('.')[-1]}"
+    del d["_target_"]
+
+    if len(d) <= 4:
+        params = [f"{str(k)}={str(v)}" for k, v in d.items()]
+        return f"{klass}({', '.join(params)})"
+    else:
+        return {f"{klass}.{str(k)}": str(v) for k, v in d.items()}
+
+
+def replace_target_with_string(d: dict) -> None:
+    for k, v in d.items():
+        if isinstance(v, dict):
+            if "_target_" in v:
+                d[k] = target_dict_to_string(v)
+            else:
+                replace_target_with_string(v)
+        elif isinstance(v, list):
+            for i, e in enumerate(v):
+                if isinstance(e, dict) and "_target_" in e:
+                    v[i] = target_dict_to_string(e)
+                elif isinstance(e, dict):
+                    replace_target_with_string(e)
 
 
 @rank_zero_only
@@ -13,35 +50,21 @@ def log_hyperparameters(
     data_module: DataModule,
     trainer: Trainer,
 ) -> None:
-    hp = {}
+    hp = deepcopy(OmegaConf.to_container(config, resolve=True))
+    replace_target_with_string(hp)
+    replace_list_with_dict(hp)
 
-    hp["seed"] = config.seed
-
-    hp["data"] = config.data
-    hp["data/num_classes"] = len(data_module.label_hierarchy.classes)
-    hp["data/num_train_samples"] = len(data_module.train_dataset)
-    hp["data/num_val_samples"] = len(data_module.val_dataset)
-    hp["data/num_test_samples"] = len(data_module.test_dataset)
-
-    for name in ["train", "test"]:
-        # todo: as one list
-        # del hp[f"data/{name}_transforms"]
-        for i, cfg in enumerate(config.data[f"{name}_transforms"]):
-            # todo: add kwargs
-
-            hp[f"data/{name}_transform_{i}"] = f"{cfg['_target_'].split('.')[-1]}"
-
-    hp["model"] = config.model
-    hp["model/num_parameters"] = sum(p.numel() for p in lit_module.parameters())
-
-    # TODO: add everything to here, probably flattened?
-    # TODO: how to deal with all these class instances?
+    hp["data"]["num_classes"] = len(data_module.label_hierarchy.classes)
+    hp["data"]["num_train_samples"] = len(data_module.train_dataset)
+    hp["data"]["num_val_samples"] = len(data_module.val_dataset)
+    hp["data"]["num_test_samples"] = len(data_module.test_dataset)
+    hp["model"]["num_parameters"] = sum(p.numel() for p in lit_module.parameters())
 
     # it says it cant, but it works fine with a dict
     trainer.logger.log_hyperparams(hp)  # type: ignore
 
     # hacky trick found on the internet
-    def empty(*args, **kwargs):
+    def null(*args, **kwargs):
         pass
 
-    trainer.logger.log_hyperparams = empty
+    trainer.logger.log_hyperparams = null
