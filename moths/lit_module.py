25453bce069bc334d32e6d5f25a0165907237b04
Kay Lamerigts <k.lamerigts@gmail.com>
2021-12-06 20:40:37 +0100

    hacky to put loss weights on cuda

--- moths/lit_module.py
+++ moths/lit_module.py
@@ -103,14 +103,14 @@ class LitModule(pl.LightningModule):
             for metric in self.metrics[phase][label]:
                 metric.to("cuda")
 
-        self._loss_weights.to("cuda")
+        self._loss_weights = self._loss_weights.to("cuda")
 
     def loss_fn(self, y_hat: Tensor, y: Tensor) -> Tensor:
         # torch.clone because otherwise it crashes, bug?!
         losses = [self._loss_fn(y_hat[i], torch.clone(y[i])) for i in [0, 1, 2, 3]]
 
         if self._loss_weights.device != losses[0].device:
-            self._loss_weights.to(losses[0].device)
+            self._loss_weights = self._loss_weights.to(losses[0].device)
 
         print([l.device for l in losses])
         print(self._loss_weights.device)
